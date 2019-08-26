9b23dbb0ad74ea3ff1b994f73d49de03df631067
Nicole Eichert <nicole.eichert@psy.ox.ac.uk>
2019-08-25 20:54:35 -0400

    add comments to data_viz

--- data_visualization/example_catplot.py
+++ data_visualization/example_catplot.py
@@ -4,28 +4,33 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
-# Relationship between continuous and categorical data
+# --------
+# overhead
+# --------
 rootdir = 'my/path/somewhere/'
 subs = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10']
 ROI_list = ['ROI1', 'ROI2', 'ROI3', 'ROI4']
 condition_list = ['pre', 'post']
 hemi_list = ["L", "R"]
 
+# --------
+# read data into dataframe
+# --------
 df = pd.DataFrame(columns=["subj", "ROI", "hemi", "condition", "my_value"])
-print('read in data')
 my_row = 0
 for sub in subs:
     OD = os.path.join('rootdir', 'derivatives', 'sub-' + sub)
     for ind_r, ROI in enumerate(ROI_list):
         for hemi in hemi_list:
             for ind_c, cond in enumerate(condition_list):
-                # use random value here as example
+                # generate random value here as example
                 my_val = np.random.randint(10) + ind_r + ind_c
                 df.loc[my_row] = [sub, ROI, hemi, cond, my_val]
                 my_row = my_row + 1
 
-print('make plot')
-
+# --------
+# plotting
+# --------
 # boxes show quartiles
 # whiskers show rest of distribution (here within 2 SD)
 sns.catplot(x="ROI", y="my_value", data=df, dodge=True, hue='condition', col='hemi', kind='box', whis=[5, 95], aspect=3)
--- data_visualization/example_slider.py
+++ data_visualization/example_slider.py
@@ -1,18 +1,22 @@
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider
-import os
 import pandas as pd
 import seaborn as sns
 
-# Relationship between continuous and categorical data
+# --------
+# overhead
+# --------
 rootdir = 'my/path/somewhere/'
 subs = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10']
 thres_list = np.arange(0, 101)
 hemi_list = ["L", "R"]
 
+# --------
+# read data into dataframe
+# --------
 df = pd.DataFrame(columns=["subj", "hemi", "threshold", "my_value"])
-print('read in data')
 my_row = 0
 for sub in subs:
     OD = os.path.join('rootdir', 'derivatives', 'sub-' + sub)
@@ -23,33 +27,41 @@ for sub in subs:
             df.loc[my_row] = [sub, hemi, thr, my_val]
             my_row = my_row + 1
 
+#  initial threshold level chosen for plotting
+thres_init = 50
 
-fig = plt.figure()
-ax = fig.add_subplot(111)
-# Adjust the subplots region to leave some space for the sliders and buttons
+# initialize matplotlib figure
+fig, ax = plt.subplots(figsize=(4, 4))
+# adjust the subplots region to leave some space for the sliders and buttons
 fig.subplots_adjust(bottom=0.25)
-
-t_init = 50
-
-# Define an axes area and draw a slider in it
+# define an axes area and draw a slider in it
 my_slider_ax = fig.add_axes([0.25, 0.1, 0.65, 0.03])
-my_slider = Slider(my_slider_ax, 'threshold (%)', 0, 100, valinit=t_init)
+# generate slider with initial value
+my_slider = Slider(my_slider_ax, 'threshold (%)', 0, 100, valinit=thres_init)
 
 
-# Define an action for modifying the plot when any slider's value changes
-def sliders_on_changed(val):
-    update_plot(ax, np.round(val))
+# define an action for when the slider's value changes
+def slider_action(val):
+    # the figure is updated when the slider is changed
+    update_plot(np.round(val))
+
+
+# link slider_action function to slider object
+my_slider.on_changed(slider_action)
+
+
+# define how to update the plot
+def update_plot(thres):
+    # clear the axis before the plot is redrawn
+    ax.clear()
+    sns.boxplot(x="hemi", y="my_value", data=df[df.threshold == thres], ax=ax)
+    # keep the axis limits constant for better visibility of the changes
+    ax.set_ylim(0, np.max(df.my_value.values))
+    # update figure
     fig.canvas.draw_idle()
 
 
-def update_plot(ax, thres):
-    ax.clear()
-    sns.boxplot(x="hemi", y="my_value", data=df[df.threshold == thres], ax=ax)
-    ax.set_ylim(0, np.max(df.my_value.values))
-
-
-update_plot(ax, t_init)
-
-my_slider.on_changed(sliders_on_changed)
+# draw initial plot with default threshold
+update_plot(thres_init)
 
 plt.show()
