b0e9f8d3a329341ab9e20a835d102d901f24a7b7
Nicole Eichert <nicole.eichert@psy.ox.ac.uk>
2020-04-04 23:28:01 +0200

    fix fame sizes

--- network/gui.py
+++ network/gui.py
@@ -14,6 +14,7 @@ import pandas as pd
 class MainApplication:
     def __init__(self, master):
         self.master = master
+
         master.title("My network")
         dd = os.path.join(os.sep, 'Users', 'neichert', 'code', 'projects', 'network')
         self.nodes_orig = pd.read_csv(os.path.join(dd, 'nodes.csv'))
@@ -27,31 +28,32 @@ class MainApplication:
         self.cbuts = []
         self.tick_vals = {}
 
-        self.frame_tick = tk.Frame(root)
-        self.frame_tick.pack(side=tk.LEFT)
-        self.text = tk.Text(self.frame_tick, cursor="arrow")
-        self.vsb = tk.Scrollbar(self.frame_tick, command=self.text.yview)
-        self.text.configure(yscrollcommand=self.vsb.set)
-        self.text.pack(side=tk.LEFT, expand=True)
-        self.vsb.pack()
+        self.frame_tick = tk.Frame(self.master)
+        self.frame_tick.grid(row=0, column=0, sticky=tk.W)
+        self.text = tk.Text(self.frame_tick)
+        self.text.grid(row=0, column=0, sticky=tk.W)
+
+        self.vsb = tk.Scrollbar(self.frame_tick)
+        self.text.configure(width=30, yscrollcommand=self.vsb.set)
+        self.vsb.grid(row=0, column=0, sticky=tk.E)
 
         self.create_cbuts()
         self.update_nodes()
 
-        self.frame_buttons = tk.Frame(root)
-        self.frame_buttons.pack()
+        self.frame_buttons = tk.Frame(self.master)
+        self.frame_buttons.grid(row=1, column=0, sticky=tk.W)
 
         self.btn_select = tk.Button(self.frame_buttons, text='select all', command=self.select_all_cbuts)
-        self.btn_select.pack(side=tk.RIGHT)
+        self.btn_select.grid()
 
         self.btn_deselect = tk.Button(self.frame_buttons, text='deselect all', command=self.deselect_all_cbuts)
-        self.btn_deselect.pack(side=tk.RIGHT)
+        self.btn_deselect.grid()
 
         self.btn_refresh = tk.Button(self.frame_buttons, text='refresh', command=self.click_refresh)
-        self.btn_refresh.pack(side=tk.RIGHT)
+        self.btn_refresh.grid()
 
-        self.frame_network = tk.Frame(root)
-        self.frame_network.pack(side=tk.RIGHT)
+        self.frame_network = tk.Frame(self.master)
+        self.frame_network.grid(row=0, column=1, rowspan=2, sticky=tk.W)
 
         self.generate_network()
         self.draw_network()
@@ -60,33 +62,31 @@ class MainApplication:
         self.edges = self.edges_orig[(self.edges_orig['node1'].isin(self.nodes.name)) | (self.edges_orig['node2'].isin(self.nodes.name))]
         g = nx.from_pandas_edgelist(self.edges, 'node1', 'node2', edge_attr=True)
         g.add_nodes_from(self.nodes.name)
-        plt.show()
         self.network = g
 
     def draw_network(self):
         plt.close("all")
         for widget in self.frame_network.winfo_children():
             widget.destroy()
-        fig = Figure(figsize=(10, 8), dpi=100)
+        fig = Figure(figsize=(8, 8))
         ax = fig.add_subplot(111)
         nx.draw(self.network, with_labels=True, ax=ax, edge_color=self.edges['colour'], node_size=10)
         canvas = FigureCanvasTkAgg(fig, master=self.frame_network)
         canvas.draw()
-        canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+        canvas.get_tk_widget().grid(sticky=tk.NSEW)
 
     def create_cbuts(self):
         for ind, row in self.nodes_orig.iterrows():
             var = tk.IntVar()
             cb = tk.Checkbutton(self.text, text=row['name'], variable=var)
             self.cbuts.append(cb)
-            self.cbuts[-1].pack()
+            self.cbuts[-1].grid()
             if row['tick_var'] == 1:
                 self.cbuts[-1].select()
             self.tick_vals[row['name']] = var
             self.text.window_create("end", window=cb)
             self.text.insert("end", "\n")
         self.text.configure(state="disabled")
-
 
     def update_nodes(self):
         ind_include = np.array([])
