f60bb4e16b78fc72fb7afd66e712e65d3846e285
Nicole Eichert <nicole.eichert@psy.ox.ac.uk>
2020-04-04 20:42:24 +0200

    refresh works

--- network/gui.py
+++ network/gui.py
@@ -4,61 +4,97 @@ from matplotlib.backends.backend_tkagg import (
 # Implement the default Matplotlib key bindings.
 from matplotlib.backend_bases import key_press_handler
 from matplotlib.figure import Figure
-import networkx as nx
 import matplotlib.pyplot as plt
+import networkx as nx
 import os
 import numpy as np
 import pandas as pd
 
-DD = os.path.join(os.sep, 'Users', 'neichert', 'code', 'projects', 'network')
 
-nodes = pd.read_csv(os.path.join(DD, 'nodes.csv'))
-node_names = nodes.name.tolist()
-edges = pd.read_csv(os.path.join(DD, 'edges.csv'))
-edges['weight'] = np.where(edges.category == 'partner', 2, 3)
-edge_pairs_w = [tuple(x) for x in edges[['node1', 'node2', 'weight']].values]
-#node_names = list(set(edges[['node1', 'node2']].values.reshape(-1).tolist()))
+class MainApplication:
+    def __init__(self, master):
+        self.master = master
+        master.title("My network")
+        dd = os.path.join(os.sep, 'Users', 'neichert', 'code', 'projects', 'network')
+        self.nodes_orig = pd.read_csv(os.path.join(dd, 'nodes.csv'))
+        self.edges_orig = pd.read_csv(os.path.join(dd, 'edges.csv'))
+        self.edges_orig['weight'] = np.where(self.edges_orig.category == 'partner', 2, 3)
 
-G = nx.Graph()
-G.add_nodes_from(nodes)
-G.add_weighted_edges_from(edge_pairs_w)
+        self.nodes = []
+        self.edges = []
+        self.network = []
+        self.cbuts = []
+        self.tick_vals = {}
 
-root = tk.Tk()
-root.wm_title("My network")
+        self.frame_network = tk.Frame(root)
+        self.frame_network.pack(side=tk.LEFT)
 
-fig = Figure(figsize=(5, 4), dpi=100)
-t = np.arange(0, 3, .01)
-ax = fig.add_subplot(111)
-nx.draw(G, with_labels=True, ax=ax)
+        self.frame_tick = tk.Frame(root)
+        self.frame_tick.pack(side=tk.RIGHT)
 
-frame_network = tk.Frame(root)
-frame_network.pack(side=tk.LEFT)
+        self.create_cbuts()
+        self.update_nodes()
 
-canvas = FigureCanvasTkAgg(fig, master=frame_network)  # A tk.DrawingArea.
-canvas.draw()
-canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+        self.btn_refresh = tk.Button(self.frame_tick, text='refresh', command=self.click_refresh)
+        self.btn_refresh.pack()
 
-bottomframe = tk.Frame(root)
-bottomframe.pack(side=tk.RIGHT)
+        self.generate_network()
+        self.draw_network()
 
-# initialize nodes tickbox list
-tick_vars = {}
-for node_name in node_names:
-    node = nodes[nodes.name == node_name]
-    var = tk.IntVar()
-    cb = tk.Checkbutton(bottomframe, text=node.name.values[0], variable=var)
-    if node.tick_var.values[0] == 1:
-        cb.select()
-    cb.pack()
-    tick_vars[node_name] = var
+    def generate_network(self):
+        #self.edges = self.edges_orig
+        self.edges = self.edges_orig[(self.edges_orig['node1'].isin(self.nodes.name)) | (self.edges_orig['node2'].isin(self.nodes.name))]
+        edge_pairs_w = [tuple(x) for x in self.edges[['node1', 'node2', 'weight']].values]
+        g = nx.Graph()
+        g.add_nodes_from(self.nodes.name)
+        g.add_weighted_edges_from(edge_pairs_w)
+        self.network = g
+
+    def draw_network(self):
+        plt.close("all")
+        for widget in self.frame_network.winfo_children():
+            widget.destroy()
+        fig = Figure(figsize=(5, 4), dpi=100)
+        ax = fig.add_subplot(111)
+        nx.draw(self.network, with_labels=True, ax=ax)
+        canvas = FigureCanvasTkAgg(fig, master=self.frame_network)
+        canvas.draw()
+        canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+
+    def create_cbuts(self):
+        for ind, row in self.nodes_orig.iterrows():
+            var = tk.IntVar()
+            self.cbuts.append(tk.Checkbutton(self.frame_tick, text=row['name'], variable=var))
+            self.cbuts[-1].pack()
+            if row['tick_var'] == 1:
+                self.cbuts[-1].select()
+            self.tick_vals[row['name']] = var
 
 
-def check_tick_vars():
-    values = [(node_name, var.get()) for node_name, var in tick_vars.items()]
-    print(values)
+    def update_nodes(self):
+        ind_include = np.array([])
+        for ind, row in self.nodes_orig.iterrows():
+            if self.tick_vals[row['name']].get():
+                ind_include = np.append(ind_include, ind)
+        self.nodes = self.nodes_orig.iloc[ind_include]
 
-button = tk.Button(bottomframe, text="Quit", command=root.quit())
-button.pack()
+    def deselect_all_cbuts(self):
+        for i in self.cbuts:
+            i.var = 1
+            i.select()
 
-check_tick_vars()
-tk.mainloop()
+    def select_all_cbuts(self):
+        for i in self.cbuts:
+            i.var = 0
+            i.deselect()
+
+    def click_refresh(self):
+        self.update_nodes()
+        self.generate_network()
+        self.draw_network()
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    MainApplication(root)
+    root.mainloop()
