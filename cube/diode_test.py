e7a3a2347ff984161ee228dd6f00ff26b1653003
Nicole Eichert <nicole.eichert@psy.ox.ac.uk>
2020-11-08 12:17:45 +0000

    colour example

--- cube/diode_test.py
+++ cube/diode_test.py
@@ -5,39 +5,27 @@ import numpy as np
 import mpl_toolkits.mplot3d.axes3d as p3
 from matplotlib import animation
 
-'''src: https://stackoverflow.com/questions/51632254/animated-3d-python-plot-with-several-moving-points'''
 
-fig = plt.figure()
-ax = p3.Axes3D(fig)
+def main():
+    numframes = 100
+    numpoints = 10
 
-q = [[-4.32, -2.17, -2.25, 4.72, 2.97, 1.74],
-     [2.45, 9.73,  7.45, 4.01, 3.42,  1.80],
-     [-1.40, -1.76, -3.08, -9.94, -3.13, -1.13]]
+    rgb_color_data = np.random.random((numpoints, 3))
+    x, y = np.random.random((2, numpoints))
 
-fx = np.array(q[0])
-fy = np.array(q[1])
-fz = np.array(q[2])
+    fig = plt.figure()
+    scat = plt.scatter(x, y, c=rgb_color_data, s=100) #this work well at this level
 
-fpoints, = ax.plot(fx, fy, fz, '*')
-txt = fig.suptitle('')
+    ani = animation.FuncAnimation(fig, update_plot2, frames=range(numframes),
+                                  fargs=(rgb_color_data, scat))
 
-
-def update_points(num, x, y, z, points):
-    # update title
-    txt.set_text('num={:d}'.format(num))
-    # calculate the new sets of coordinates here. The resulting arrays should have the same shape
-    # as the original x,y,z
-    new_x = x + np.random.normal(1, 0.1, size=(len(x),))
-    new_y = y + np.random.normal(1, 0.1, size=(len(y),))
-    new_z = z + np.random.normal(1, 0.1, size=(len(z),))
-
-    # update properties
-    points.set_data(new_x, new_y)
-    points.set_3d_properties(new_z, 'z')
-
-    # return modified artists
-    return points, txt
+    plt.show()
 
 
-ani = animation.FuncAnimation(fig, update_points, frames=30, fargs=(fx, fy, fz, fpoints))
-plt.show()
+def update_plot2(i,data,scat):
+    data[ i%10 ] = np.random.random((3))
+    scat.set_color(data)
+    return scat,
+
+
+main()
