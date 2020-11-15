88f269c4e1bbe36c0d0a5c01107b96f115c093cf
Nicole Eichert <nicole.eichert@psy.ox.ac.uk>
2020-11-15 18:55:42 +0000

    colour map fixed

--- cube/test.py
+++ cube/test.py
@@ -5,22 +5,33 @@ import numpy as np
 import mpl_toolkits.mplot3d.axes3d as p3
 from matplotlib import animation
 
-i_grid = 7
-x, y, z = np.meshgrid(np.arange(i_grid), np.arange(i_grid), np.arange(i_grid))
-fig = plt.figure()
-ax = p3.Axes3D(fig)
 
 
 def main():
-    ani = animation.FuncAnimation(fig, update_plot, frames=range(len(x)))
+    i_grid = 7
+    t_step = 500 # miliseconds
+    n_vols = 10
+    x, y, z = np.meshgrid(np.arange(i_grid), np.arange(i_grid), np.arange(i_grid))
+    fig = plt.figure()
+    ax = p3.Axes3D(fig)
+    data_in = np.zeros((i_grid, i_grid, i_grid, n_vols))
+    data_in[::2, :, :, ::2] = 1
+
+    def update_plot(i):
+        data_vol = data_in[:, :, :, i]
+        print(data_vol[0,:,0])
+        scat = ax.scatter(x.reshape(-1), y.reshape(-1), z.reshape(-1), c=data_vol.reshape(-1), cmap='binary', marker='o', depthshade=False, vmin=0.9999, vmax=1.0001)
+        ax.set_xticklabels("")
+        ax.set_yticklabels("")
+        ax.set_yticklabels("")
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+        ax.set_ylabel('z')
+        return scat
+
+    ani = animation.FuncAnimation(fig, update_plot, interval=t_step, frames=n_vols)
     plt.show()
 
-
-def update_plot(i):
-    data = np.zeros((len(x.reshape(-1)), 3))
-    data[i, :] = [1, 0, 1]
-    scat = ax.scatter(x.reshape(-1), y.reshape(-1), z.reshape(-1), c=data)
-    return scat
 
 
 main()
