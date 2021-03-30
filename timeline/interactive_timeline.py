507972a491b387e3dc9ecc339d9f684e64d4df57
Nicole Eichert <nicole.eichert@psy.ox.ac.uk>
2021-03-30 22:37:29 +0100

    fix colours

--- timeline/interactive_timeline.py
+++ timeline/interactive_timeline.py
@@ -17,24 +17,16 @@ import plotly.express as px
 pd.set_option('mode.chained_assignment', None)
 
 class timeline():
-    def __init__(self, filename=os.path.join(os.path.dirname(sys.argv[0]), 'some_dates.csv')):
-        # define scale values
-        self.scales_info = pd.DataFrame(columns=['var_name', 'xlabel', 'title', 'default_min', 'default_max', 'tot_min', 'tot_max', 'cat_order'])
-        self.scales_info.loc[len(self.scales_info)] = ['mya', 'million years ago', 'History of Life', -4600, 0, -4600, 0, ['supereon', 'eon', 'era', 'period', 'epoch', 'event']]
-        self.scales_info.loc[len(self.scales_info)] = ['year', 'year', 'Modern History', 1500, 2000, -3500, 2019, ['epoch', 'person', 'art', 'event', 'invention']]
-        self.scales_info.loc[len(self.scales_info)] = ['pre', 'kilo years ago', 'Prehistoric Time', -1000, 0, -3600, 0, ['epoch', 'event', 'invention', 'art', 'person']]
-        
-        # selected scale
-        self.scale_type = 'year'
-        self.myscale = self.get_myscale()
+    def __init__(self):
+        self.min = -3000
+        self.max = 2000
 
         # load total data file
-        self.filename = filename
-        self.df_orig = self.load_data()
-        self.dict_cat = self.get_dict_cat()
+        #self.df = self.prep_data()
+        #self.set_ypos()
 
-        # initialize selection 
-        self.df_myscale = self.get_df_myscale()
+        # or load
+        self.df = pd.read_csv(os.path.join(os.path.dirname(sys.argv[0]), 'timeline_prep.csv'))
 
         # plot
         self.do_plot()
@@ -44,96 +36,60 @@ class timeline():
         myscale = self.scales_info[self.scales_info.var_name == scale_type].to_dict('r')[0]
         return myscale
 
-    def get_dict_cat(self):
-        # create dataframe to store information about categories
-        df_cat = pd.DataFrame({'category': self.df_orig['category'].unique()})
-        df_cat['category'] = pd.Categorical(df_cat['category'], self.myscale['cat_order'])
-        #df_cat['color'] = self.colors[0:len(df_cat)]
-        df_cat['color'] = np.arange(len(df_cat))
-        df_cat.sort_values('category', inplace=True, ignore_index=True)
-        dict_cat = df_cat.set_index('category')['color'].to_dict()
-        return dict_cat
-
-    def load_data(self):
-        df = pd.read_csv(self.filename)
-        # replace missing yearOff by yearOn
-        df['yearOff'] = np.where(np.isnan(df['yearOff']), df['yearOn'], df['yearOff'])
-        df['yearOn'] = df['yearOn'].astype(pd.Int64Dtype())
-        df['yearOff'] = df['yearOff'].astype(pd.Int64Dtype())
-        # ignore empty column
-        df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
-        return df
-
-    def get_df_myscale(self):
-        # filter the desired scale
-        df = self.df_orig[self.df_orig.scale == self.scale_type]
-        if self.scale_type == 'mya':
-            df.yearOn = -df.yearOn
-            df.yearOff = -df.yearOff
-        elif self.scale_type == 'pre':
-            df.yearOn = np.round(df.yearOn / 1000)
-            df.yearOff = np.round(df.yearOff / 1000)
-            df['yearOn'] = df['yearOn'].astype(pd.Int64Dtype())
-            df['yearOff'] = df['yearOff'].astype(pd.Int64Dtype())
-
-        df = df.loc[(df['yearOn'] >= self.myscale['tot_min']) & (df['yearOff'] + 1 < self.myscale['tot_max'])]
-
+    def prep_data(self):
+        df = pd.read_csv(os.path.join(os.path.dirname(sys.argv[0]), 'timeline.csv'))
+        df_cat = pd.read_csv(os.path.join(os.path.dirname(sys.argv[0]), 'timeline_types.csv'))
+        # replace missing date_to by date_from
+        df['date_to'] = np.where(np.isnan(df['date_to']), df['date_from'], df['date_to'])
+        df['date_from'] = df['date_from'].astype(pd.Int64Dtype())
+        df['date_to'] = df['date_to'].astype(pd.Int64Dtype())
+        df = df.loc[(df['date_from'] >= self.min) & (df['date_to'] + 1 < self.max)]
         # add column for duration of event (min 1 year)
-        df['duration'] = df['yearOff'] - df['yearOn']
+        df['duration'] = df['date_to'] - df['date_from']
         df['duration'] = np.where(df.duration < 1, 1, df['duration'])
-        # add task-column
-        df['ypos'] = self.get_ypos(df)
-        # sort and re-index cleaned df
-        df['category'] = pd.Categorical(df['category'], self.myscale['cat_order'])
-        # column for color value
-        df['color'] = df['category']
-        df.replace({"color": self.dict_cat}, inplace=True)
-        # sort
-        df = df.sort_values(['category', 'yearOn'])
+        # map category
+        di = df_cat.set_index('timeline_type_id')['timeline_type_name'].to_dict()
+        df['timeline_type_name'] = df['timeline_type_id'].map(di)
+        df = df.sort_values(['timeline_type_id', 'date_from'])
         df.index = pd.RangeIndex(len(df.index))
         return df
 
-    def new_scale(self):
-        self.myscale = self.get_myscale()
-        self.df_myscale = self.get_df_myscale()
-
-    def get_ypos(self, df):
+    def set_ypos(self):
         # initialize parameters
         ypos_arr = np.array([0])
         ypos_group = 0
         ymax = 0
         # loop over categories
-        for cat in self.dict_cat.keys():
-            group = df[df.category == cat]
+        for cat in np.unique(self.df['timeline_type_id'].values):
+            print(cat)
+            group = self.df[self.df.timeline_type_id == cat]
             filled = pd.DataFrame(columns=['ypos', 'on', 'off'])
             # draw event as rectangle
             for ind, row in group.iterrows():
                 # draw the event in the next free row
                 ypos = ypos_group + 1
-                # add subsequent pathes directly
-                if cat in ['supereon', 'eon', 'era', 'period', 'epoch']:
-                    while any((filled['ypos'] == ypos) & (((filled['on'] < (row['yearOn'])) & ((row['yearOn']) < filled['off'])) | ((filled['on'] < (row['yearOff'])) & ((row['yearOn'] + row['duration']) < filled['off'])))):
-                        ypos += 1
-                # start new row for short events
-                else:
-                    while any((filled['ypos'] == ypos) & (((filled['on'] < (row['yearOn'] - 1)) & ((row['yearOn'] - 1) < filled['off'])) | ((filled['on'] < (row['yearOff'] - 1)) & ((row['yearOn'] + row['duration'] + 1) < filled['off'])))):
-                        ypos += 1
+                while any((filled['ypos'] == ypos) & (((filled['on'] < (row['date_from'] - 1)) & ((row['date_from'] - 1) < filled['off'])) | ((filled['on'] < (row['date_to'] - 1)) & ((row['date_from'] + row['duration'] + 1) < filled['off'])))):
+                    ypos += 1
                 if ypos > ymax:
                     ymax = ypos
-                filled = filled.append({'ypos': ypos, 'on': row['yearOn'], 'off': row['yearOn'] + row['duration']}, ignore_index=True)
+                filled = filled.append({'ypos': ypos, 'on': row['date_from'], 'off': row['date_from'] + row['duration']}, ignore_index=True)
                 ypos_arr = np.append(ypos_arr, ypos)
             # start a new cateory in a new row
             ypos_group = ymax
         ypos_arr = np.delete(ypos_arr, 0)
-        return ypos_arr
+        self.df['ypos'] = ypos_arr
+        self.df.to_csv(os.path.join(os.path.dirname(sys.argv[0]), 'timeline_prep.csv'))
 
     def do_plot(self):
-        fig = px.timeline(self.df_myscale, x_start="yearOn", x_end="yearOff", y="ypos", hover_name="title", hover_data=["yearOn", "yearOff"], color='color', color_continuous_scale='Viridis')
+        fig = px.timeline(self.df, x_start="date_from", x_end="date_to", y="ypos", hover_name="text_raw", hover_data=["date_from", "date_to"], color='timeline_type_name')
         fig.layout.xaxis.type = 'linear'
-        fig.data[0].x = self.df_myscale.duration.tolist()
+        for i_d, dat in enumerate(fig.data):
+            fig.data[i_d].x = self.df[self.df['timeline_type_name'] == dat.name].duration.tolist()
         fig.update_layout(xaxis=dict(rangeslider=dict(visible=True), type="linear"))
-        fig.update_layout(hovermode="x")
-        fig['layout']['yaxis']['autorange'] = "reversed"
+        fig.layout.yaxis.autorange = "reversed"
+        fig.layout.yaxis.visible = False
+        fig.update_layout(title_text="Interactive timeline")
+
         fig.show()
 
 # mainloop
